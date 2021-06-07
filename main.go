02102bc658984e3274e76c482c444632a63a6c38
lee <1019978548@qq.com>
2021-06-07 15:45:59 +0800

    fist commit1

--- main.go
+++ main.go
@@ -5,18 +5,37 @@ import (
 	"net/http"
 )
 
-func main()  {
+// 定义接收数据的结构体
+type Login struct {
+	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+	User    string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Pssword string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
+
+func main() {
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
 	r := gin.Default()
-	//限制上传最大尺寸
-	r.MaxMultipartMemory = 8 << 20
-	r.POST("/upload", func(c *gin.Context) {
-		file, err := c.FormFile("file")
-		if err != nil {
-			c.String(500, "上传图片出错")
+	// JSON绑定
+	r.POST("loginJSON", func(c *gin.Context) {
+		// 声明接收的变量
+		var json Login
+		//username := c.PostForm("username")
+		//password := c.PostForm("password")
+		//c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s", username, password))
+		// 将request的body中的数据，自动按照json格式解析到结构体
+		if err := c.ShouldBindJSON(&json); err != nil {
+			// 返回错误信息
+			// gin.H封装了生成json数据的工具
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
-		// c.JSON(200, gin.H{"message": file.Header.Context})
-		c.SaveUploadedFile(file, file.Filename)
-		c.String(http.StatusOK, file.Filename)
+		// 判断用户名密码是否正确
+		if json.User != "root" || json.Pssword != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
 	})
 	r.Run(":8888")
 }
